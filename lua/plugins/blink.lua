d24c5a576bb3d292e4af1ec58fef5da50fcb4693
r4ppz <johnreyrabosa.f@gmail.com>
2026-05-18 16:09:40 +0800

    refactor(blink): improve plugin config and enhance completion options

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,94 +1,61 @@
 return {
   {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    event = "InsertEnter",
+    opts = {
+      history = true,
+      updateevents = "TextChanged,TextChangedI",
+    },
+    config = function(_, opts)
+      require("luasnip").config.set_config(opts)
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_snipmate").lazy_load()
+      require("luasnip.loaders.from_lua").lazy_load()
+    end,
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {
+      fast_wrap = {},
+      disable_filetype = { "TelescopePrompt", "vim" },
+    },
+  },
+
+  {
     "saghen/blink.cmp",
     version = "1.*",
-    event = { "InsertEnter", "CmdLineEnter" },
+    event = { "CmdLineEnter", "InsertEnter" },
 
     dependencies = {
-      {
-        "L3MON4D3/LuaSnip",
-        dependencies = { "rafamadriz/friendly-snippets" },
-        opts = {
-          history = true,
-          updateevents = "TextChanged,TextChangedI",
-        },
-        config = function(_, opts)
-          require("luasnip").config.set_config(opts)
-          require("luasnip.loaders.from_vscode").lazy_load()
-          require("luasnip.loaders.from_snipmate").lazy_load()
-          require("luasnip.loaders.from_lua").lazy_load()
-        end,
-      },
-
-      {
-        "windwp/nvim-autopairs",
-        opts = {
-          fast_wrap = {},
-          disable_filetype = { "TelescopePrompt", "vim" },
-        },
-        config = function(_, opts)
-          require("nvim-autopairs").setup(opts)
-        end,
-      },
+      "rafamadriz/friendly-snippets",
     },
 
     opts = function()
       local nv_blink = require("nvchad.blink.config")
 
       return vim.tbl_deep_extend("force", nv_blink, {
-        enabled = function()
-          local disabled_ft = {
-            TelescopePrompt = true,
-            snacks_picker_input = true,
-            ["copilot-chat"] = true,
-            markdown = true,
-          }
-
-          if vim.bo.buftype == "prompt" then
-            return false
-          end
-
-          return not disabled_ft[vim.bo.filetype]
-        end,
-
-        keymap = {
-          preset = "none",
-
-          ["<C-Up>"] = { "select_prev", "fallback" },
-          ["<C-Down>"] = { "select_next", "fallback" },
-
-          ["<S-Up>"] = { "scroll_documentation_up", "fallback" },
-          ["<S-Down>"] = { "scroll_documentation_down", "fallback" },
-
-          ["<C-S-Down>"] = { "show", "show_documentation", "hide_documentation", "fallback" },
-          ["<C-Space>"] = { "show", "show_documentation", "hide_documentation", "fallback" },
-          ["<C-c>"] = { "hide", "fallback" },
-
-          ["<CR>"] = { "accept", "fallback" },
-
-          ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
-          ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-        },
-
-        sources = {
-          default = { "lsp", "snippets", "buffer", "path" },
-
-          providers = {
-            buffer = {
-              max_items = 20,
-            },
-          },
-        },
-
         completion = {
-          list = {
-            selection = {
-              preselect = false,
-              auto_insert = false,
+          documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 500,
+            window = {
+              border = "single",
+              max_height = 15,
+              max_width = 70,
             },
           },
 
           ghost_text = { enabled = false },
+
+          list = {
+            selection = {
+              auto_insert = false,
+              preselect = false,
+            },
+          },
 
           menu = {
             draw = {
@@ -98,15 +65,70 @@ return {
                 },
               },
             },
+            max_height = 15,
+            scrollbar = false,
           },
+        },
 
-          documentation = {
-            auto_show = true,
-            auto_show_delay_ms = 200,
-            window = {
-              max_width = 70,
-              max_height = 15,
-              border = "single",
+        enabled = function()
+          local ft = vim.bo.filetype
+          local bt = vim.bo.buftype
+
+          if bt == "prompt" then
+            return false
+          end
+
+          local disabled = {
+            ["copilot-chat"] = true,
+            markdown = true,
+            snacks_picker_input = true,
+            TelescopePrompt = true,
+          }
+
+          return not disabled[ft]
+        end,
+
+        fuzzy = {
+          implementation = "prefer_rust",
+          sorts = { "kind", "exact", "score", "sort_text" },
+        },
+
+        keymap = {
+          preset = "none",
+
+          ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+          ["<C-c>"] = { "hide", "fallback" },
+          ["<C-Down>"] = { "select_next", "fallback" },
+          ["<C-e>"] = { "hide", "fallback" },
+          ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+          ["<C-S-Down>"] = { "show", "show_documentation", "hide_documentation", "fallback" },
+          ["<C-Space>"] = { "show", "show_documentation", "hide_documentation", "fallback" },
+          ["<C-Up>"] = { "select_prev", "fallback" },
+          ["<CR>"] = { "accept", "fallback" },
+          ["<S-Down>"] = { "scroll_documentation_down", "fallback" },
+          ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+          ["<S-Up>"] = { "scroll_documentation_up", "fallback" },
+          ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+        },
+
+        signature = {
+          enabled = true,
+          window = {
+            border = "single",
+            max_height = 8,
+            max_width = 80,
+          },
+        },
+
+        sources = {
+          default = { "lsp", "snippets", "buffer", "path" },
+          providers = {
+            buffer = {
+              max_items = 20,
+              min_keyword_length = 2,
+            },
+            path = {
+              min_keyword_length = 2,
             },
           },
         },
