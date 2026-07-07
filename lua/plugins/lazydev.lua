f77ca87f6402a5e4a6c12625727909b372b891f3
r4ppz <johnreyrabosa.f@gmail.com>
2026-07-07 11:24:26 +0800

    feat(lazydev): add enabled function and snacks lib and remove luarc

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -2,9 +2,16 @@ return {
   "folke/lazydev.nvim",
   ft = "lua",
   opts = {
+    -- Dynamically disable lazydev if the project manages its own LuaLS environment
+    enabled = function(root_dir)
+      return not (
+        vim.uv.fs_stat(root_dir .. "/.luarc.json") or vim.uv.fs_stat(root_dir .. "/.luarc.jsonc")
+      )
+    end,
     library = {
       { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       { path = "LazyVim", words = { "LazyVim" } },
+      { path = "snacks.nvim", words = { "Snacks" } },
       {
         path = "ui/nvchad_types",
         words = { "nvchad", "NvChad", "chadrc" },
