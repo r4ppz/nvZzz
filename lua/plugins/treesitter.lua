76e2fa0094aec10c4a7c5cf73ff5202614777207
r4ppz <johnreyrabosa.f@gmail.com>
2026-08-03 15:51:35 +0800

    refactor(treesitter): improve setup, remove tmux

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,77 +1,81 @@
+local languages = {
+  "luadoc",
+  "printf",
+  "vim",
+  "vimdoc",
+  "markdown",
+  "latex",
+  "markdown_inline",
+  "query",
+  "ini",
+  "udev",
+  "ssh_config",
+  "kitty",
+
+  "diff",
+  "git_config",
+  "gitcommit",
+  "git_rebase",
+  "gitignore",
+  "gitattributes",
+  "regex",
+  "rasi",
+
+  "sql",
+  "lua",
+  "bash",
+  "zsh",
+  "java",
+  "rust",
+  "python",
+  "c",
+  "zig",
+  "asm",
+  "cpp",
+  "hyprlang",
+  "go",
+  "gomod",
+  "gowork",
+  "gosum",
+  "php",
+  "blade",
+
+  "yaml",
+  "toml",
+  "xml",
+  "json",
+  "qmljs",
+
+  "html",
+  "css",
+  "javascript",
+  "typescript",
+  "tsx",
+  "astro",
+  "svelte",
+  "prisma",
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
   lazy = false,
-  config = function()
-    local languages = {
-      "luadoc",
-      "printf",
-      "vim",
-      "vimdoc",
-      "markdown",
-      "latex",
-      "markdown_inline",
-      "query",
-      "ini",
-      "udev",
-      "ssh_config",
-      "tmux",
-      "kitty",
-
-      "diff",
-      "git_config",
-      "gitcommit",
-      "git_rebase",
-      "gitignore",
-      "gitattributes",
-      "regex",
-      "rasi",
-
-      "sql",
-      "lua",
-      "bash",
-      "zsh",
-      "java",
-      "rust",
-      "python",
-      "c",
-      "zig",
-      "asm",
-      "cpp",
-      "hyprlang",
-      "go",
-      "gomod",
-      "gowork",
-      "gosum",
-      "php",
-      "blade",
-
-      "yaml",
-      "toml",
-      "xml",
-      "json",
-      "qmljs",
-
-      "html",
-      "css",
-      "javascript",
-      "typescript",
-      "tsx",
-      "astro",
-      "svelte",
-      "prisma",
-    }
-
+  build = function()
     require("nvim-treesitter").install(languages)
-
-    -- Enable highlighting automatically for all buffers where a parser exists
+  end,
+  config = function()
     vim.api.nvim_create_autocmd("FileType", {
       group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true }),
       callback = function(args)
         local buf = args.buf
-        local lang = vim.treesitter.language.get_lang(vim.bo[buf].filetype) or vim.bo[buf].filetype
-        local ok, _ = pcall(vim.treesitter.start, buf, lang)
-        if ok then
+        local ft = vim.bo[buf].filetype
+        if ft == "" then
+          return
+        end
+
+        local lang = vim.treesitter.language.get_lang(ft) or ft
+
+        if pcall(vim.treesitter.start, buf, lang) then
           vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         end
       end,
